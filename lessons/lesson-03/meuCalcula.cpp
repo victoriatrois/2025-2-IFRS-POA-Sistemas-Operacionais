b31b0ced3c80dda441d2083287aedd012ac8c824
Victoria Trois <vickytrois@outlook.com>
2025-08-27 21:46:21 +0000

    Adds correction for lesson 03\'s activity

--- lessons/lesson-03/meuCalcula.cpp
+++ lessons/lesson-03/meuCalcula.cpp
@@ -2,20 +2,61 @@
 #include <math.h>
 #include <pthread.h> // importei a biblio para threads
 
-double soma_massiva(long long limite) {
+typedef struct {
+  long long limite;
+  long long iterações;
+  double resultadoDaSoma;
+  double resultadoDaAproximacaoDePi;
+} IngredientesParaOperações; // para dizer que é uma struct, usamos PascalCase
+
+// double soma_massiva(long long limite) {
+//   double soma = 0.0;
+
+//   for (long long i = 1; i <= limite; i++) {
+//     soma += sqrt(i) / (i + 1.0); // operação mais pesada que apenas somar
+//   }
+
+//   return soma;
+// }
+
+// para trabalhar com threads precisamos passar por parâmetro um endereço de memória
+// nesse caso o da struct que terá o valor da variável para determinar a soma
+void* soma_massivamente_com_thread(void* arg) { // long long limite => void* arg
+  IngredientesParaOperações* argumentos = (IngredientesParaOperações*)arg;
   double soma = 0.0;
 
-  for (long long i = 1; i <= limite; i++) {
+  for (long long i = 1; i <= argumentos->limite; i++) {
     soma += sqrt(i) / (i + 1.0); // operação mais pesada que apenas somar
   }
 
-  return soma;
+  argumentos->resultadoDaSoma = soma;
+
+  return NULL;
 }
 
-double aproximar_pi(long long iteracoes) {
+// double aproximar_pi(long long iteracoes) {
+//   double pi = 0.0;
+
+//   for (long long i = 0; i < iteracoes; i++) {
+//     double termo;
+
+//     if (i % 2 == 0) {
+//       termo = 1.0 / (2.0 * i + 1.0);
+//     } else {
+//       termo = -1.0 / (2.0 * i + 1.0);
+//     }
+
+//     pi += termo;
+//   }
+
+//   return pi * 4.0;
+// }
+
+void* aproxima_pi_com_thread(void* arg) {
+  IngredientesParaOperações* argumentos = (IngredientesParaOperações*)arg;
   double pi = 0.0;
 
-  for (long long i = 0; i < iteracoes; i++) {
+  for (long long i = 0; i < argumentos->iterações; i++) {
     double termo;
 
     if (i % 2 == 0) {
@@ -26,30 +67,38 @@ double aproximar_pi(long long iteracoes) {
 
     pi += termo;
   }
+  argumentos->resultadoDaAproximacaoDePi = pi;
 
-  return pi * 4.0;
+  return NULL;
 }
 
-void *soma_massiva_pthread(void *arg) { soma_massiva(*(long long *)arg); return NULL; }
-void *aproximar_pi_pthread(void *arg) { aproximar_pi(*(long long *)arg); return NULL; }
+// void *soma_massiva_pthread(void *arg) { soma_massiva(*(long long *)arg); return NULL; }
+// void *aproximar_pi_pthread(void *arg) { aproximar_pi(*(long long *)arg); return NULL; }
 
 int main() {
-  long long n1 = 5000000000;
-  long long n2 = 10000000000;
+  long long numeroParaSomarMassivamente = 5000000000;
+  long long numeroParaProximarDePi = 10000000000;
 
-  pthread_t thread1, thread2;
+  pthread_t threadParaSomaMassiva, threadParaAproximarDePi;
+  IngredientesParaOperações argumentos;
+  argumentos.limite = numeroParaSomarMassivamente;
+  argumentos.iterações = numeroParaProximarDePi;
 
   // printf("Iniciando o Procesos de calculos massivos Sequencial...\n");
   printf("Iniciando os Procesos de calculos massivos e aproximação de Pi de forma paralela...\n");
 
-  printf("Calculos Soma massivos ...\n");
-  pthread_create(&thread1, NULL, soma_massiva_pthread, &n1);
+  printf("Calculos Soma massivos...\n");
+  pthread_create(&threadParaSomaMassiva, NULL, soma_massivamente_com_thread, (void*)&argumentos);
 
-  printf("Calculos aproximação do Pi ...\n");
-  pthread_create(&thread2, NULL, aproximar_pi_pthread, &n2);
+  printf("Calculos aproximação do Pi...\n");
+  pthread_create(&threadParaAproximarDePi, NULL, aproxima_pi_com_thread, (void*)&argumentos);
 
-  pthread_join(thread1, NULL);
-  pthread_join(thread2, NULL);
+  pthread_join(threadParaSomaMassiva, NULL);
+  pthread_join(threadParaAproximarDePi, NULL);
+
+  printf("Resultado da soma massiva: %.6f\n", argumentos.resultadoDaSoma);
+  printf("Resultado da aproximação de Pi: %.12f\n", argumentos.resultadoDaAproximacaoDePi);
+
   printf("Calculos finalizados.\n");
 
   return 0;
